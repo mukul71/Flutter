cb18d730ee9de5ee3d2670b62e37299fe7f75dc8
Mukul Rahman <mukul71@users.noreply.github.com>
2023-11-15 15:47:42 +0600

    Update 3_Simple_form.dart
    submit button added

--- 3_Simple_form.dart
+++ 3_Simple_form.dart
@@ -8,54 +8,78 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    theme: ThemeData(
-      colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blueGrey),
-          scaffoldBackgroundColor: Colors.white70,
-         appBarTheme: AppBarTheme(backgroundColor: Colors.brown,
-         foregroundColor: Colors.white70),
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(backgroundColor: Colors.brown),
-    ),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blueGrey),
+        scaffoldBackgroundColor: Colors.white70,
+        appBarTheme: AppBarTheme(
+            backgroundColor: Colors.brown, foregroundColor: Colors.white70),
+        bottomNavigationBarTheme:
+            BottomNavigationBarThemeData(backgroundColor: Colors.brown),
+      ),
       home: HomePage(),
     );
   }
 }
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    mySnackBar(message,context){
-      return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+    mySnackBar(message, context) {
+      return ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(message)));
     }
+
+    ButtonStyle buttonStyle =
+        ElevatedButton.styleFrom(minimumSize: Size(300, 60));
+
     return Scaffold(
       appBar: AppBar(
         title: Text('My App'),
         actions: [
-          IconButton(onPressed: (){mySnackBar('Mail me', context);}, icon: Icon(Icons.mail)),
-        IconButton(onPressed: (){mySnackBar('Write a comment', context);}, icon: Icon(Icons.comment)),
-          IconButton(onPressed: (){mySnackBar('Search here', context);}, icon: Icon(Icons.search)),
-          IconButton(onPressed: (){mySnackBar('This is Settings', context);}, icon: Icon(Icons.settings))
+          IconButton(
+              onPressed: () {
+                mySnackBar('Mail me', context);
+              },
+              icon: Icon(Icons.mail)),
+          IconButton(
+              onPressed: () {
+                mySnackBar('Write a comment', context);
+              },
+              icon: Icon(Icons.comment)),
+          IconButton(
+              onPressed: () {
+                mySnackBar('Search here', context);
+              },
+              icon: Icon(Icons.search)),
+          IconButton(
+              onPressed: () {
+                mySnackBar('This is Settings', context);
+              },
+              icon: Icon(Icons.settings))
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){mySnackBar('I am FAB', context);},
+        onPressed: () {
+          mySnackBar('I am FAB', context);
+        },
         child: Icon(Icons.check),
       ),
       bottomNavigationBar: BottomNavigationBar(
-
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.local_gas_station),label: 'Station'),
-          BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.desk),label: 'Counter')
+          BottomNavigationBarItem(
+              icon: Icon(Icons.local_gas_station), label: 'Station'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.desk), label: 'Counter')
         ],
         currentIndex: 1,
-        onTap: (index){
-          if(index == 0){
+        onTap: (index) {
+          if (index == 0) {
             mySnackBar('Station', context);
-          }else if(index == 1){
+          } else if (index == 1) {
             mySnackBar('Home', context);
-          }else if(index ==2){
+          } else if (index == 2) {
             mySnackBar('Cash Counter', context);
           }
         },
@@ -63,10 +87,32 @@ class HomePage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Padding(padding: EdgeInsets.all(20), child: TextField(decoration: InputDecoration(border:OutlineInputBorder(),labelText: 'First name'),),),
-          Padding(padding: EdgeInsets.all(20),child: TextField(decoration: InputDecoration(border:OutlineInputBorder(),labelText: 'Last name' ),),),
-          Padding(padding: EdgeInsets.all(20),child: TextField(decoration: InputDecoration(border :OutlineInputBorder(),labelText: 'Email'),),),
-          ElevatedButton(onPressed: (){}, child: Text('Submit'),style: ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'First name'),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Last name'),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Email'),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Submit'),
+            style: buttonStyle,
+          ),
         ],
       ),
     );
