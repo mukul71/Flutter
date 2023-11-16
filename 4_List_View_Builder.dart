fa3b97c56f54cb838f959ca25d2264153225d240
Mukul Rahman <mukul71@users.noreply.github.com>
2023-11-16 14:07:19 +0600

    Update 4_List_View_Builder.dart
    list view added

--- 4_List_View_Builder.dart
+++ 4_List_View_Builder.dart
@@ -25,11 +25,20 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     mySnackBar(message,context){
       return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message),duration: Duration(seconds: 1),));
     }
 
+  var myList = [
+    {"image":"https://rabbil.com/files/flutterS.png","title":"Saiful"},
+    {"image":"https://rabbil.com/files/flutterS.png","title":"Rifat"},
+    {"image":"https://rabbil.com/files/flutterS.png","title":"Riyad"},
+    {"image":"https://rabbil.com/files/flutterS.png","title":"Porag"},
+    {"image":"https://rabbil.com/files/flutterS.png","title":"Abrar"},
+    {"image":"https://rabbil.com/files/flutterS.png","title":"Medha"}
 
+  ];
 
 
     return Scaffold(
@@ -61,6 +70,20 @@ class HomeScreen extends StatelessWidget {
           }else if(index == 2){
             mySnackBar('Business', context);
           }
+        },
+      ),
+      body: ListView.builder(
+        itemCount: myList.length,
+        itemBuilder: (context,index){
+          return GestureDetector(
+            onTap: (){mySnackBar(myList[index]["title"], context);},
+            child: Container(
+              height: 200,
+              width: double.infinity,
+              padding: EdgeInsets.all(10),
+              child: Image.network(myList[index]["image"]!,fit: BoxFit.fill,),
+            ),
+          );
         },
       ),
     );
