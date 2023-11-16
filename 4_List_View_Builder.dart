2b0531f94e088aa9927e3d460e96903593114ac6
Mukul Rahman <mukul71@users.noreply.github.com>
2023-11-16 11:23:24 +0600

    Update 4_List_View_Builder.dart
    snackbar method added

--- 4_List_View_Builder.dart
+++ 4_List_View_Builder.dart
@@ -25,13 +25,34 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    mySnackBar(message,context){
+      return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+    }
+
+
+
+
     return Scaffold(
       appBar:AppBar(
         title: Text('List View'),
+        actions: [
+          IconButton(onPressed: (){mySnackBar('Mail us', context);}, icon: Icon(Icons.mail)),
+          IconButton(onPressed: (){mySnackBar('Write a comment', context);}, icon: Icon(Icons.comment)),
+          IconButton(onPressed: (){mySnackBar('Search here', context);}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){mySnackBar('This is Settings', context);}, icon: Icon(Icons.settings)),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.check),
         onPressed: (){},
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.shop),label: 'Shop'),
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.business),label: 'Business'),
+        ],
+        currentIndex: 1,
       ),
     );
   }
