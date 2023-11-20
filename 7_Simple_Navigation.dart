9a5c516b79d1be5771ba4ebcaf019cec7f48b0b2
Mukul Rahman <mukul71@users.noreply.github.com>
2023-11-20 11:26:49 +0600

    Update 7_Simple_Navigation.dart
    page one added

--- 7_Simple_Navigation.dart
+++ 7_Simple_Navigation.dart
@@ -28,3 +28,17 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:Text('Page 1')
+      ),
+    );
+  }
+}
+
